cfa0a26ec08052e8488ae51a2aaa082b02dc7805
mlportius <mlportius@gmail.com>
2025-01-30 03:36:34 -0300

    INIT REPO

---
+++ app.py
@@ -0,0 +1,121 @@
+import os
+import sys
+import io
+import gdown
+import traceback
+import streamlit as st
+from scripts import prepare_data
+
+# Inicializar la variable de estado
+if "download_flag" not in st.session_state:
+    st.session_state.download_flag = False
+
+if "prepared_flag" not in st.session_state:
+    st.session_state.prepared_flag = False
+
+if os.getcwd().replace('\\', '/').split('/')[-1] != 'python':
+    os.chdir('python/')
+
+st.image(".configs/banner.png")
+
+st.subheader('CONTEO DE COLOCALIZACION:rocket::moon:')
+
+#%% Configurar carpetas
+
+basefolder = ''
+ofolder = basefolder + 'origen/'
+infolder = basefolder + 'input/'
+outfolder = basefolder + 'output/'
+
+#%% Cargar datos
+
+# url = 'https://drive.google.com/file/d/1PJlQGSkdYcaaK6B8Bhf1Hs2dzo-I00WI/view?usp=drive_link'
+# nombre_archivo = 'SYN09 DS 080124'
+
+url = st.text_input("Url de Google Drive", "https://drive.google.com/file/d/...")
+nombre_archivo = st.text_input("Nombre del Archivo", "miarchivo.zip")
+
+def download_data(output_folder, url, nombre_archivo):
+    
+    # corrijo la url
+    file_id = url.split('d/')[-1].split('/view')[0]
+    url_fix = f'https://drive.google.com/uc?id={file_id}&confirm=t'
+
+    # corrijo el nombre
+    if not nombre_archivo.endswith('.zip'):
+        nombre_archivo += '.zip'
+
+    # decargo archivo
+    savename = f'{output_folder}{nombre_archivo}'
+    gdown.download(url_fix, savename, quiet=False)
+
+    return savename
+
+#st.button("Reset", type="primary")
+if st.button("Download"):
+
+    if os.path.exists(ofolder + nombre_archivo):
+        st.write('Archivo ya descargado!')
+        st.session_state.download_flag = True
+
+    if st.session_state.download_flag == False:
+
+        with st.spinner('Descargando...'):
+            try: 
+                download_data(ofolder, url, nombre_archivo)
+                st.session_state.download_flag = True
+                st.success('       ... done!')
+            except Exception as e:
+                st.error('       ... error!')
+                st.text(traceback.format_exc())
+
+if st.session_state.download_flag:
+
+    on = st.toggle("Carpeta ya descomprimida")
+
+    if st.button("Preparar datos"):
+
+        with st.spinner('Preparando...'):
+
+            # Capturar la salida estándar
+            old_stdout = sys.stdout
+            sys.stdout = mystdout = io.StringIO()
+            
+            try: 
+                if on == False:
+                    prepare_data.run(True, True)
+                else:
+                    prepare_data.run(False, True)
+                
+                # Restaurar la salida estándar y mostrar el resultado
+                sys.stdout = old_stdout
+                st.text(mystdout.getvalue())
+                
+                st.session_state.prepared_flag = True 
+
+                st.success('       ... done!')
+                
+            except Exception as e:
+
+                # Restaurar la salida estándar y mostrar el resultado
+                sys.stdout = old_stdout
+                st.text(mystdout.getvalue())
+
+                st.error('       ... error!')
+                st.text(traceback.format_exc())
+
+
+    if st.session_state.prepared_flag:
+        
+        archivo_excel = outfolder + os.listdir(outfolder)[-1]
+
+        with open(archivo_excel, "rb") as f:
+
+            # Botón de descarga
+            st.download_button(
+                label="📥 Descargar Excel",
+                data=f,
+                file_name=os.listdir(outfolder)[-1],
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
+    
---
+++ scripts/prepare_data.py
@@ -0,0 +1,235 @@
+def run(descomprimir=True, verbose=False):
+        
+    import os
+    import pandas as pd
+    import tqdm
+    import zipfile
+
+    if verbose: print('Trabajando archivo ...')
+
+    #%% DEFINIR FUNCIONES
+
+    def descomprimir_zip(archivo_origen, carpeta_destino):
+
+        with zipfile.ZipFile(archivo_origen, 'r') as zip_ref:
+            zip_ref.extractall(carpeta_destino)
+
+        return True
+
+
+    def SAVE_EXCEL_MULTISHEET(container, path, engine='openpyxl', ind=False):
+            
+            import numpy as np
+            import pandas as pd
+
+            ext = path.split('.')[-1]
+            if ext != 'xlsx':
+                print('\nERROR: Use .xlsx path...')
+
+            else:
+
+                if type(container) == dict:
+                    
+                    keys  = list(container.keys())
+                    
+                    with pd.ExcelWriter(path) as writer:
+                        print('Writing to {}'.format(path))
+                        for k in keys:
+                            print('     ...',k,'sheet')
+                            container[k].to_excel(writer, sheet_name=k, index=ind, engine=engine)
+                        print('     ... done!')
+                
+                elif type(container) == list:
+                    
+                    with pd.ExcelWriter(path) as writer:
+                        print('Writing to {}'.format(path))
+                        for i in np.arange(len(container)):
+                            k = 'sheet' + str(i)
+                            print('     ...', k)
+                            container[i].to_excel(writer, sheet_name=k, index=ind, engine=engine)
+                        print('     ... done!')
+
+    #%% CONFIGURAR ENTORNO
+
+    if verbose: print('     ... configurando entorno')
+
+    archivo_orgien_nombre =  os.listdir('origen/')[0]
+    archivo_origen = 'origen/' + os.listdir('origen/')[0]
+    carpeta_destino = 'input/'
+
+    nombre_muestra = archivo_origen.split('/')[-1].split('.zip')[0]
+
+    nombre_muestra_sin_fecha = nombre_muestra.split(' ')[:2]
+    nombre_muestra_sin_fecha = ' '.join([str(x) for x in nombre_muestra_sin_fecha])
+
+    fecha_toma_muestra = nombre_muestra.split(' ')[-1]
+    fecha_toma_muestra = fecha_toma_muestra[0:2] + '-' + fecha_toma_muestra[2:4] + '-20' + fecha_toma_muestra[4:7]
+
+
+    #%% DESCOMPRIMIR ARCHIVO
+
+    if verbose: print('     ... descomprimiento carpeta origen')
+
+    if descomprimir: descomprimir_zip(archivo_origen = archivo_origen, carpeta_destino=carpeta_destino)
+    
+    if os.listdir(carpeta_destino)[0] != nombre_muestra:
+        os.rename(carpeta_destino + os.listdir(carpeta_destino)[0], carpeta_destino + nombre_muestra)
+    
+    carpeta_input = carpeta_destino + nombre_muestra + '/'
+
+    #%% RECONOCER BUCLES
+
+    if verbose: print('     ... reconociendo bucles')
+
+    # reconocer carpetas
+    carpetas = [x for x in os.listdir(carpeta_input) if not x.endswith('.nd2')]
+
+    # reconocer condiciones
+    condiciones = [x.split(nombre_muestra)[-1][1:] for x in carpetas]
+    condiciones = [x.split('cubre')[0][:-1] for x in condiciones]
+    condiciones = list(set(condiciones))
+
+    # reconocer cubres de cada condicion
+    dicto = {}
+
+    for cond in condiciones:
+
+        cubres = [x.split(nombre_muestra)[-1][1:] for x in carpetas]
+        cubres = [x for x in cubres if cond in x]
+        cubres = [x.split('cubre')[-1][1:] for x in cubres]
+        cubres = [x.split(' ')[0] for x in cubres]
+        cubres = list(set(cubres))
+
+        dicto[cond] = cubres
+
+    # reconocer stacks de cada cubre de cada condicion
+    stacks_01 = [x.split(nombre_muestra)[-1][1:] for x in carpetas]
+
+    dicto01 = {}
+
+    for cond in condiciones:
+
+        stacks_02 = [x for x in stacks_01 if cond in x]
+
+        dicto02 = {}
+        
+        for cubre in cubres:
+            stacks_03 = [x.split(f'cubre {cubre}')[-1][1:] for x in stacks_02 if f'cubre {cubre}' in x]
+            dicto02[cubre] = stacks_03
+
+        dicto01[cond] = dicto02
+
+    #%% PREPARAR TABLAS
+
+    if verbose: print('     ... preparando tablas')
+
+    tablas = {}
+
+    condiciones = list(dicto01.keys())
+    condiciones = sorted(condiciones)
+
+    for t in condiciones:
+
+        if t == 'GRK5 GRK6 TH':
+            variables = ['TH', 'GRK5', 'GRK6', 'GRK5/TH', 'GRK6/TH', 'GRK5-GRK6/TH']
+        elif t == 'GRK2 GRK3 TH':
+            variables = ['TH', 'GRK2', 'GRK3', 'GRK2/TH', 'GRK3/TH', 'GRK2-GRK3/TH']
+        elif t == 'Control Negativo':
+            variables = ['647', '488', 'Cy3', '488/647', 'Cy3/647', '488-Cy3/647']
+
+        valor0 = None
+        valor1 = None
+        valor2 = None
+        valor3 = None
+        valor4 = None
+        valor5 = None
+
+        filas = []
+
+        cubres = list(dicto01[t].keys())
+
+        for c in cubres:
+
+            stacks = list(dicto01[t][c])
+
+            for s in stacks:
+                
+                # busco carpeta
+                carpeta_analisis = 'input/'
+                carpeta_analisis = carpeta_analisis + nombre_muestra + '/' + nombre_muestra
+                carpeta_analisis = carpeta_analisis + ' ' + t
+                carpeta_analisis = carpeta_analisis + ' ' + 'cubre' + ' ' + c
+                carpeta_analisis = carpeta_analisis + ' ' + s + '/'
+
+                # leo archivos
+                archivos = os.listdir(carpeta_analisis)
+                archivos = [x for x in archivos if not x.endswith(('.tif', '.csv'))]
+                archivos = [x for x in archivos if not 'Segmentacion' in x]
+                archivos = sorted(archivos)
+
+                a1 = archivos[0]
+                a2 = archivos[1]
+                a3 = archivos[2]
+
+                # extraigo informacion
+                with open(carpeta_analisis + a1, 'r') as f:
+                    a1_text = f.readlines()
+                    a1_text = [int(x.replace('\n', '').split(' =')[-1]) for x in a1_text]
+
+                valor0 = a1_text[0]
+                valor1 = a1_text[1]
+                valor3 = a1_text[2]
+
+                with open(carpeta_analisis + a2, 'r') as f:
+                    a2_text = f.readlines()
+                    a2_text = [int(x.replace('\n', '').split(' =')[-1]) for x in a2_text]
+
+                valor5 = a2_text[2]
+
+                with open(carpeta_analisis + a3, 'r') as f:
+                    a3_text = f.readlines()
+                    a3_text = [int(x.replace('\n', '').split(' =')[-1]) for x in a3_text]
+
+                valor2 = a3_text[1]
+                valor4 = a3_text[2]
+
+                valores = [valor0, valor1, valor2, valor3, valor4, valor5]
+
+                # creo fila
+                fila = {}
+                fila['Fecha'] = fecha_toma_muestra
+                fila['Stack'] = s
+                fila['Cubre'] = int(c)
+                fila['Región del cerebro'] = nombre_muestra_sin_fecha
+
+                for v in variables:
+                    indice = variables.index(v)
+                    fila[v] = valores[indice]
+
+                fila = pd.Series(fila)
+
+                filas.append(fila)
+
+        filas = pd.concat(filas, axis=1).T
+        #ilas['Fecha'] = pd.to_datetime(filas['Fecha'], format='%d-%m-%Y')
+        filas = filas.sort_values(by=['Cubre', 'Stack'])
+        
+        tablas[nombre_muestra_sin_fecha + ' ' + t] = filas
+
+    #%% EXPORTAR EXCEL
+
+    if verbose: print('     ... exportando excel')
+
+    cond_outtext = condiciones
+    cond_outtext = [x for x in cond_outtext if 'Control' not in x]
+    cond_outtext = sorted(cond_outtext)
+    cond_outtext = [x.replace(' TH', '') for x in cond_outtext]
+    cond_outtext = ' '.join(cond_outtext) + ' TH'
+
+    nombre_archivo = f'Datos {nombre_muestra_sin_fecha} {cond_outtext}'
+
+    filename = f'output/{nombre_archivo}.xlsx'
+    SAVE_EXCEL_MULTISHEET(tablas, filename)
+
+
+    print('     ... done!')
